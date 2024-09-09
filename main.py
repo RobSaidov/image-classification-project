1626d906e64f2ced6215a20e96072866dd64ca32
RobSaidov <robsaidov@gmail.com>
2024-09-08 23:03:01 -0700

    saved the model and tested on different photos, so far we have the perfect performance

--- main.py
+++ main.py
@@ -27,28 +27,36 @@ for i in range(25):
 
 
 
-#Build the model
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (32,32,3)))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation = 'relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation = 'relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation = 'relu'))
-model.add(layers.Dense(10, activation = 'softmax'))
+# #Build the model
 
-model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
-
-model.fit(training_images, training_labels, epochs = 10, validation_data = (testing_images, testing_labels))
-
-loss, accuracy = model.evaluate(testing_images, testing_labels)
-print(f"Loss = {loss}")
-print(f'Accuracy = {accuracy} ')
-
-model.save('image_classifier.keras')
-# plt.imshow(training_images[1])
-# plt.show()
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (32,32,3)))
+# model.add(layers.MaxPooling2D((2,2)))
+# model.add(layers.Conv2D(64, (3,3), activation = 'relu'))
+# model.add(layers.MaxPooling2D((2,2)))
+# model.add(layers.Conv2D(64, (3,3), activation = 'relu'))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation = 'relu'))
+# model.add(layers.Dense(10, activation = 'softmax'))
 #
-# print(training_images.shape)
-# print(training_labels.shape)
+# model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+#
+# model.fit(training_images, training_labels, epochs = 10, validation_data = (testing_images, testing_labels))
+#
+# loss, accuracy = model.evaluate(testing_images, testing_labels)
+# print(f"Loss = {loss}")
+# print(f'Accuracy = {accuracy} ')
+#
+# model.save('image_classifier.keras')
+
+
+model = keras.models.load_model('image_classifier.keras')
+
+img = cv.imread('ship.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+plt.imshow(img, cmap = plt.cm.binary)
+
+prediction = model.predict(np.array([img])/255)
+index = np.argmax(prediction)
+print(f'It is a {class_names[index]}')
